7c4494876630500072477bbfc050fb696216827d
aniketsarapAI <aniketsarap27@gmail.com>
2024-11-25 13:20:17 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,19 @@
-from setuptools import setup
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-## edit below variables as per your requirements -
-REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
-AUTHOR_USER_NAME = "saurabhznaikz"
-SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit']
-
+REPO_NAME = "Personalized-Movie-Recommendation-System"
+AUTHOR_USER_NAME = "aniketsarap"
+SRC_REPO = "movie_recommender"
+LIST_OF_REQUIREMENTS = ['streamlit', 'scikit-learn', 'numpy', 'pandas']
 
 setup(
     name=SRC_REPO,
-    version="0.0.1",
+    version="0.0.2",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A personalized package for a Movie Recommendation System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    author_email="entbappy73@gmail.com",
+    author_email="aniketsarap27@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=LIST_OF_REQUIREMENTS
 )
